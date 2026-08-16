a176536b61d2e2fcd6381bdc6ea393d4d54db10b
danielrincon084-pixel <danielrincon084@gmail.com>
2026-08-16 06:19:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,19 @@
-self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = 'lifequest-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
